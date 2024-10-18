39084adf169cd50d52a355ce5903462f02b5bd26
Julio Rodriguez <julio@estratos.net>
2024-10-18 01:34:16 -0600

    add functions

--- src/HestiaApi.php
+++ src/HestiaApi.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Estratos\HestiaApi;
+
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class HestiaApi extends AbstractBundle
@@ -29,7 +30,7 @@ class HestiaApi extends AbstractBundle
         $hst_username = 'admin';
         $hst_password = $this->password;
         $hst_command = 'v-list-user';
-        
+
         // Prepare POST query
         $postvars = array(
             'user' => $hst_username,
@@ -49,7 +50,7 @@ class HestiaApi extends AbstractBundle
         return $data;
     }
 
-    public function listUsers( $format = 'json')
+    public function listUsers($format = 'json')
     {
         // Server credentials
         $hst_hostname = $this->hostname;
@@ -58,14 +59,14 @@ class HestiaApi extends AbstractBundle
         $hst_username = 'admin';
         $hst_password = $this->password;
         $hst_command = 'v-list-sys-users';
-     
+
 
         // Prepare POST query
         $postvars = array(
             'user' => $hst_username,
             'password' => $hst_password,
             'returncode' => $hst_returncode,
-            'cmd' => $hst_command,                 
+            'cmd' => $hst_command,
             'arg1' => $format
         );
 
@@ -78,6 +79,106 @@ class HestiaApi extends AbstractBundle
         return $data;
     }
 
+    public function addUser($username, $password, $email,  $format = 'json')
+    {
+        // Server credentials
+        $hst_hostname = $this->hostname;
+        $hst_port = '8083';
+        $hst_returncode = 'no';
+        $hst_username = 'admin';
+        $hst_password = $this->password;
+        $hst_command = 'v-add-user';
+
+        // Prepare POST query
+        $postvars = array(
+            'user' => $hst_username,
+            'password' => $hst_password,
+            'returncode' => $hst_returncode,
+            'cmd' => $hst_command,
+            'arg1' => $username,
+            'arg2' => $password,
+            'arg3' => $email,
+            'arg4' => $format
+        );
+
+        $answer = $this->request($postvars);
+
+        // Parse JSON output
+        $data = json_decode($answer, true);
+
+
+        return $data;
+    }
+
+
+
+
+    public function suspendUser($username, $format = 'json')
+    {
+
+
+        //// bin/v-suspend-user
+        // Server credentials
+        $hst_hostname = $this->hostname;
+        $hst_port = '8083';
+        $hst_returncode = 'no';
+        $hst_username = 'admin';
+        $hst_password = $this->password;
+        $hst_command = 'v-suspend-user';
+
+        // Prepare POST query
+        $postvars = array(
+            'user' => $hst_username,
+            'password' => $hst_password,
+            'returncode' => $hst_returncode,
+            'cmd' => $hst_command,
+            'arg1' => $username,
+            'arg2' => $format
+        );
+
+
+
+        $answer = $this->request($postvars);
+
+        // Parse JSON output
+        $data = json_decode($answer, true);
+
+
+        return $data;
+    }
+    public function unsuspendUser($username, $format = 'json')
+    {
+
+
+        //// bin/v-suspend-user
+        // Server credentials
+        $hst_hostname = $this->hostname;
+        $hst_port = '8083';
+        $hst_returncode = 'no';
+        $hst_username = 'admin';
+        $hst_password = $this->password;
+        $hst_command = 'v-unsuspend-user';
+
+        // Prepare POST query
+        $postvars = array(
+            'user' => $hst_username,
+            'password' => $hst_password,
+            'returncode' => $hst_returncode,
+            'cmd' => $hst_command,
+            'arg1' => $username,
+            'arg2' => $format
+        );
+
+
+
+        $answer = $this->request($postvars);
+
+        // Parse JSON output
+        $data = json_decode($answer, true);
+
+
+        return $data;
+    }
 
     public function listDomain($username = "user", $domain, $format = 'json')
     {
@@ -88,7 +189,7 @@ class HestiaApi extends AbstractBundle
         $hst_username = 'admin';
         $hst_password = $this->password;
         $hst_command = 'v-list-web-domain';
-        
+
         // Prepare POST query
         $postvars = array(
             'user' => $hst_username,
@@ -118,14 +219,14 @@ class HestiaApi extends AbstractBundle
         $hst_username = 'admin';
         $hst_password = $this->password;
         $hst_command = 'v-list-web-domains';
-        
+
         // Prepare POST query
         $postvars = array(
             'user' => $hst_username,
             'password' => $hst_password,
             'returncode' => $hst_returncode,
             'cmd' => $hst_command,
-            'arg1' => $username,            
+            'arg1' => $username,
             'arg2' => $format
         );
 
@@ -173,6 +274,182 @@ class HestiaApi extends AbstractBundle
             return "Query returned error code: " . $answer . "\n";
         }
     }
+
+    ///// MAIL FUNCIONS
+    public function listMailDomains($username, $format = 'json')
+    {
+
+
+        //// bin/v-list-mail-domains
+        // Server credentials
+        $hst_hostname = $this->hostname;
+        $hst_port = '8083';
+        $hst_returncode = 'no';
+        $hst_username = 'admin';
+        $hst_password = $this->password;
+        $hst_command = 'v-list-mail-domains';
+
+        // Prepare POST query
+        $postvars = array(
+            'user' => $hst_username,
+            'password' => $hst_password,
+            'returncode' => $hst_returncode,
+            'cmd' => $hst_command,
+            'arg1' => $username,
+            'arg2' => $format
+        );
+
+
+
+        $answer = $this->request($postvars);
+
+        // Parse JSON output
+        $data = json_decode($answer, true);
+
+
+        return $data;
+    }
+
+    public function listMailDomain($username, $domain, $format = 'json')
+    {
+
+
+        //// bin/v-list-mail-domain
+        // Server credentials
+        $hst_hostname = $this->hostname;
+        $hst_port = '8083';
+        $hst_returncode = 'no';
+        $hst_username = 'admin';
+        $hst_password = $this->password;
+        $hst_command = 'v-list-mail-domain';
+
+        // Prepare POST query
+        $postvars = array(
+            'user' => $hst_username,
+            'password' => $hst_password,
+            'returncode' => $hst_returncode,
+            'cmd' => $hst_command,
+            'arg1' => $username,
+            'arg2' => $domain,
+            'arg3' => $format
+        );
+
+
+
+        $answer = $this->request($postvars);
+
+        // Parse JSON output
+        $data = json_decode($answer, true);
+
+
+        return $data;
+    }
+
+    public function listMailAccounts($domain, $format = 'json')
+    {
+
+
+        //// bin/v-list-mail-domains
+        // Server credentials
+        $hst_hostname = $this->hostname;
+        $hst_port = '8083';
+        $hst_returncode = 'no';
+        $hst_username = 'admin';
+        $hst_password = $this->password;
+        $hst_command = 'v-list-mail-accounts';
+
+        // Prepare POST query
+        $postvars = array(
+            'user' => $hst_username,
+            'password' => $hst_password,
+            'returncode' => $hst_returncode,
+            'cmd' => $hst_command,
+            'arg1' => $domain,
+            'arg2' => $format
+        );
+
+
+
+        $answer = $this->request($postvars);
+
+        // Parse JSON output
+        $data = json_decode($answer, true);
+
+
+        return $data;
+    }
+
+    public function changeMailaccountPassword($username, $domain,  $account, $password,  $format = 'json')
+    {
+        //// bin/v-change-mail-account-password
+        // Server credentials
+        $hst_hostname = $this->hostname;
+        $hst_port = '8083';
+        $hst_returncode = 'no';
+        $hst_username = 'admin';
+        $hst_password = $this->password;
+        $hst_command = 'v-change-mail-account-password';
+
+        // Prepare POST query
+        $postvars = array(
+            'user' => $hst_username,
+            'password' => $hst_password,
+            'returncode' => $hst_returncode,
+            'cmd' => $hst_command,
+            'arg1' => $username,
+            'arg2' => $domain,
+            'arg3' => $account,
+            'arg4' => $password,
+            'arg5' => $format
+        );
+
+        $answer = $this->request($postvars);
+
+        // Parse JSON output
+        $data = json_decode($answer, true);
+
+
+        return $data;
+    }
+
+    public function addMailaccount($username, $domain,  $account, $password,  $format = 'json')
+    {
+        //// bin/v-add-mail-account
+        // Server credentials
+        $hst_hostname = $this->hostname;
+        $hst_port = '8083';
+        $hst_returncode = 'no';
+        $hst_username = 'admin';
+        $hst_password = $this->password;
+        $hst_command = 'v-add-mail-account';
+
+        // Prepare POST query
+        $postvars = array(
+            'user' => $hst_username,
+            'password' => $hst_password,
+            'returncode' => $hst_returncode,
+            'cmd' => $hst_command,
+            'arg1' => $username,
+            'arg2' => $domain,
+            'arg3' => $account,
+            'arg4' => $password,
+            'arg5' => $format
+        );
+
+        $answer = $this->request($postvars);
+
+        // Parse JSON output
+        $data = json_decode($answer, true);
+
+
+        return $data;
+    }
+
+
+
+
+
+
 
     ///// DNS RECORD
 
@@ -232,7 +509,7 @@ class HestiaApi extends AbstractBundle
      * Set the value of port
      *
      * @return  self
-     */ 
+     */
     public function setPort($port)
     {
         $this->port = $port;
